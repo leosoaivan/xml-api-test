29a740b1b396f5b02a5c711160695928601806d3
Leonard Soai-Van <leo.soaivan@gmail.com>
2022-03-04 20:51:12 -0500

    Create super simple views

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 
 const app = express();
 const PORT = 3000;
 
+// Middleware
+app.engine('handlebars', handlebars.engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello, world')
+  res.render('index')
+})
+app.get('/articles', (req, res) => {
+  res.render('articles/index')
+})
+app.get('/authors', (req, res) => {
+  res.render('authors/index')
 })
 
+
+// Listen
 app.listen(PORT, () => {
   console.log(`Example add listening on port ${PORT}`)
 })
