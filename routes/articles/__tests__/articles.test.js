269d2236383d976546d93acc3469d026c06f57a5
Leonard Soai-Van <leo.soaivan@gmail.com>
2022-03-05 14:07:51 -0500

    Cleanup and add comments inside articles route test

--- routes/articles/__tests__/articles.test.js
+++ routes/articles/__tests__/articles.test.js
@@ -7,17 +7,16 @@ const app = new express();
 app.use('/articles', articles)
 app.engine('handlebars', handlebars.engine())
 app.set('view engine', 'handlebars')
-app.set('views', './views')
 
-xdescribe('Routes - Articles', () => {
-  beforeEach(() => {
-    fetchAndParseXMLMock = jest.spyOn(articles, 'fetchAndParseXML').mockResolvedValue({ feed: {}})
-  })
+describe('Routes - Articles', () => {
+  /**
+   * TODO:
+   * As written, this currently makes an actual fetch request from fetchAndParseXML inside
+   * indexHandler. Spying on and mocking fetchAndParseXML doesn't work. This might require a
+   * global mock of node-fetch to prevent real requests during tests.
+   */
   test('Responds to /articles', async () => {
     const response = await request(app).get('/articles')
-
-    console.log('RESPONSE: ', response)
-
     expect(response.statusCode).toBe(200)
   })
 })
