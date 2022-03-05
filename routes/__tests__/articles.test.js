574efb5730e856ae2795afc73368e0181c65be58
Leonard Soai-Van <leo.soaivan@gmail.com>
2022-03-04 22:59:05 -0500

    Add test file

---
+++ routes/__tests__/articles.test.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const request = require('supertest')
+const articles = require('../articles')
+
+describe('Routes - Articles', () => {
+  test('Responds to /articles', async () => {
+    // Some test here
+  })
+})
